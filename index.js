c259cb9fda4c1c549474d58020318571e6956413
Farshy <farihafarshy2020@gmail.com>
2025-11-28 01:06:30 +0600

    do some backend function and include it with client section

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const admin = require("firebase-admin");
 const serviceAccount = require("./ecoTrackPrivateKey.json");
-const app = express()
-const port = 3000
 
-app.use(cors())
-app.use(express.json())
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-
-
-
+// Initialize Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-
-
+// MongoDB Connection
 const uri = "mongodb+srv://ecoTrackDB:bgJTc0YxXD8TkvGU@cluster0.fh8zolv.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,88 +30,85 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    await admin.auth().verifyIdToken(token);
+    next();
+  } catch (error) {
+    res.status(401).send({ message: "unauthorized access" });
+  }
+};
+
 async function run() {
   try {
-
     await client.connect();
-
 
     const db = client.db("ecoTrackDB");
     const cardsCollection = db.collection("cards");
-    // const usersCollection = db.collection("userChallenges");
+    const joinChallengeCollection = db.collection("join-challenges");
 
+    
 
-    //  cards
+    // Get all cards
     app.get('/cards', async (req, res) => {
-      const cursor = cardsCollection.find();
-      const result = await cursor.toArray();
+      const result = await cardsCollection.find().toArray();
       res.send(result);
     });
 
-
-    // user challenge
-    // app.get('/userChallenge/:id', async (req, res) => {
-    //   const id = req.params;
-    //   const objectId = new ObjectId(id);
-    //   const result = await usersCollection.insertOne({ _id: objectId });
-    //   res.send(result);
-    // });
-
-
-    const verifyToken = async (req, res, next) => {
-      const authorization = req.headers.authorization;
-      const token = authorization.split(' ')[1];
-
-      if (!token) {
-        return res.status(401).send({message: 'unauthorized access'});
-      }
-      
-      try {
-       await admin.auth().verifyIdToken(token)
-        next();
-      } catch (error) {
-        res.status(401).send({message: 'unauthorized access'});
-      }
-      
-    }
-
-
-    app.get('/cards/:id', verifyToken ,async (req, res) => {
-      const id = req.params;
+    // Get card by ID
+    app.get('/cards/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
       const objectId = new ObjectId(id);
       const result = await cardsCollection.findOne({ _id: objectId });
-      res.send({
-        success: true,
-        result
-      });
+      res.send({ success: true, result });
     });
 
-
-    // post method
+    // Create new card
     app.post('/cards', async (req, res) => {
       const newCard = req.body;
       const result = await cardsCollection.insertOne(newCard);
-      res.send({
-        success: true,
-        result
-      }
-      );
+      res.send({ success: true, result });
     });
 
+    // Get activities for user
+    app.get('/myChallenges', verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const result = await cardsCollection.find({ createdBy: email }).toArray();
+      res.send(result);
+    });
 
+    // JOIN CHALLENGE
+    app.post('/join-challenges', async (req, res) => {
+      const challenge = req.body;
+      const result = await joinChallengeCollection.insertOne(challenge);
+      res.send(result);
+    });
 
+     app.get('/my-activities', verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const result = await joinChallengeCollection.find({ joinedBy: email }).toArray();
+      res.send(result);
+    });
+
+    // Test DB connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    console.error("Server crashed:", error);
   }
 }
+
 run().catch(console.dir);
 
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
